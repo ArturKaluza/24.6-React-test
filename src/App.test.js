87bca921763a797da73242a88fd08e00357fc658
Artur <akaluza26@gmail.com>
2018-03-18 13:16:48 +0100

    Added remove player functionality and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 import PlayersList from './components/PlayerList/PlayersList';
 import AddPlayer from './components/AddPlayer/AddPlayer';
@@ -25,7 +25,7 @@ it("should update player score", () => {
   expect(playersAfterUpdate[0].score).toEqual(10);
 });
 
-it('', () => {
+it('check player name, new player score and numbers of players', () => {
   const appComponent = shallow(<App />);
 
   const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
@@ -36,4 +36,20 @@ it('', () => {
   expect(players.length).toEqual(1);
   expect(players[0].name).toEqual('Ania');
   expect(players[0].score).toEqual(0);
+});
+
+it('check numbers of players after remove one', () => {
+  const appComponent = mount(<App />);
+  //console.log(appComponent.debug());
+
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Jan');
+  onPlayerAdd('Katarzyna');
+  
+  const onPlayerRemove = appComponent.find(PlayersList).prop('onPlayerRemove');
+  onPlayerRemove('Jan');
+
+  const players = appComponent.state('players');
+
+  expect(players.length).toEqual(1);
 });
